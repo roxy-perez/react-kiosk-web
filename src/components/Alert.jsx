86901e2423ccbcdaecd904a658b2882026317d1f
Roxy Pérez <necropsia32@gmail.com>
2024-04-19 21:29:38 +0100

    Fixed Tailwind class for Alert component

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,6 +1,6 @@
 export default function Alert({ children }) {
   return (
-    <div className="text-center my-2 font-bold bg-orange-crayola text-ghost-white p-3 uppercase">
+    <div className="text-center my-2 font-bold bg-orange-crayola text-ghost-white p-3 uppercase rounded-md">
       {children}
     </div>
   );
