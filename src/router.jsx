a655d35ccc05a65248f0ca0fde095846a6aac8ca
Roxy Pérez <necropsia32@gmail.com>
2024-05-02 19:47:47 +0100

    Starting with the Admin views

--- src/router.jsx
+++ src/router.jsx
@@ -4,6 +4,7 @@ import Layout from "./layouts/Layout";
 import Home from "./views/Home";
 import Login from "./views/Login";
 import Register from "./views/Register";
+import AdminLayout from "./layouts/AdminLayout";
 
 const Router = createBrowserRouter([
     {
@@ -28,7 +29,26 @@ const Router = createBrowserRouter([
             {
                 path: "/auth/register",
                 element: <Register />,
+            },
+            {
+                path: "/admin",
+                element: <AdminLayout />,
+                children: [
+                    {
+                        index: true,
+                        element: <Orders />,
+                    },
+                    {
+                        path: "/products",
+                        element: <Products />,
+                    },
+                    {
+                        path: "/categories",
+                        element: <Categories />,
+                    },
+                ]
             }
+
         ]
     },
 ]);
